b629f14fc7ed8666035485a83621dc1f583ea1ed
ibrahimfakhry <Ibrahim fakhrey@github.com>
2023-06-21 10:04:19 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,73 @@
+# print("programming is fun ")
+# x="ali"
+# print(x)
+# x=2
+# print(x)
+# x= input("what is your name \n")
+# if x=="ahmed":
+#     print("Hi")
+#     print("welcome")
+# else:
+#     print("error ")
+#     print("error not ahmed ")
+
+# x=int(input("how old are you \n"))
+# if x >15:
+#     print("go get an ID")
+# else:
+#     print("still young")
+# x = input("what is your name \n")
+# if x == "ahmed":
+#     f = input("enter your father name\n")
+#     if f == "emad":
+#         age = int(input("please ya ahmed enter your age \n"))
+#         if age > 15:
+#             print("great your big")
+#         else:
+#             print("oh still young")
+#     else:
+#         print("please make sure from your father name ")
+# else:
+#     print("incorrect user name ")
+# number=int(input("please enter a number \n"))
+# if number>1:
+#     for i in range (2,number):
+#         if number %i==0:
+#             print("this number is not prim ")
+#             break
+#         else:
+#             print("this number is prim ")
+#
+#
+# else:
+#     print("this is a not prime number ")
+
+# names=["ahmed","omar","ali","abdullah"]
+# for name in names:
+#     if name=="ali":
+#         print("rejected")
+#     else:
+#         print(name)
+
+# numbers=[1,2,3,4,5,6,7,8,9,10]
+# game_is_on=True
+# while game_is_on:
+#     if len(numbers)>=15:
+#         game_is_on=False
+#     search=int(input("enter a num"))
+#     # if search in numbers:
+#     #     print("found")
+#     # else:
+#     #     print("not found")
+#     found=False
+#     for n in numbers:
+#         if n==search:
+#             found=True
+#             break
+#     if found:
+#         print("found")
+#     else:
+#         print("not")
+#         numbers.append(search)
+#         print(numbers)
+
