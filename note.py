3406a3a68a68f14e32f0b7a92a22d0c2665c4bf2
Ibrahim fakhrey <77210469+ibrahimfakhrey@users.noreply.github.com>
2023-07-13 00:15:39 +0300

    Update note.py

--- note.py
+++ note.py
@@ -163,6 +163,53 @@ def dived():
         c=int(input("enter the number you want to check \n"))
         print(check(c))
 dived()
+question_data = [
+    {
+        "category": "Science: Computers",
+        "type": "boolean",
+        "difficulty": "medium",
+        "question": "The HTML5 standard was published in 2014.",
+        "correct_answer": "True",
+        "incorrect_answers": [
+            "False"
+        ]
+    },
+    {
+        "category": "Science: Computers",
+        "type": "boolean",
+        "difficulty": "medium",
+        "question": "The first computer bug was formed by faulty wires.",
+        "correct_answer": "False",
+        "incorrect_answers": [
+            "True"
+        ]
+    },
+    {
+        "category": "Science: Computers",
+        "type": "boolean",
+        "difficulty": "medium",
+        "question": "FLAC stands for 'Free Lossless Audio Condenser'.",
+        "correct_answer": "False",
+        "incorrect_answers": [
+            "True"
+        ]
+    },
+    {
+        "category": "Science: Computers",
+        "type": "boolean",
+        "difficulty": "medium",
+        "question": "All program codes have to be compiled into an executable file in order to be run. This file can then be executed on any machine.",
+        "correct_answer": "False",
+        "incorrect_answers": [
+            "True"
+        ]
+    },
+    {
+        "category": "Science: Computers",
+        "type": "boolean",
+        "difficulty": "easy",
+        "question": "Linus Torvalds created Linux and Git.",
+        "correct_answer": "True",
 "incorrect_answers": [
             "False"
         ]
